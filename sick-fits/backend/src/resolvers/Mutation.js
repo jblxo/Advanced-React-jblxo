54c1ea09060dc6d0648dda2c79f182de18b01015
Ondřej Hliba <hliba.ondra@live.com>
2018-11-15 22:20:45 +0100

    Add Signup On The Backend

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,6 +1,9 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
-    // TODO: Check if they are logged in
+    //TODO: Check if they are logged in
 
     const item = await ctx.db.mutation.createItem(
       {
@@ -34,7 +37,7 @@ const Mutations = {
     // 1. find the item
     const item = await ctx.db.query.item({ where }, `{ id title }`);
     // 2. check if theu own that item, or have the permissions
-    // TODO
+    // TODO:
     // 3. Delete it!
     return ctx.db.mutation.deleteItem(
       {
@@ -42,6 +45,33 @@ const Mutations = {
       },
       info
     );
+  },
+  async signup(parent, args, ctx, info) {
+    // lowercase their email
+    args.email = args.email.toLowerCase();
+    // hash their password
+    const password = await bcrypt.hash(args.password, 10);
+    // create the user in the database
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] }
+        }
+      },
+      info
+    );
+
+    // create the JWT for them
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // We set the JWT as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+    });
+
+    return user;
   }
 };
 
